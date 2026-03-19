48b5fc3417e61eefe9b51dccf455b3dfa9b23e33
Adarsh Aravind <adarsharavindachutham@gmail.com>
2026-03-19 16:36:33 +0530

    feat: Convert .help command to use embeds

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,13 +1,20 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
     name: 'help',
     description: 'Lists all available commands.',
     async execute(message, args, client) {
-        const commandsList = client.commands
-            .map(cmd => `**\.${cmd.name}**: ${cmd.description}`)
-            .join('\n');
-        
-        await message.reply({
-            content: `**🤖 Available Commands:**\n\n${commandsList}`
+        const embed = new EmbedBuilder()
+            .setColor('#2B2D31')
+            .setTitle('🤖 Chaotic Bot Commands')
+            .setDescription('Here is a list of all available commands:')
+            .setFooter({ text: 'Requested by ' + message.author.tag, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+            .setTimestamp();
+
+        client.commands.forEach(cmd => {
+            embed.addFields({ name: `.${cmd.name}`, value: cmd.description, inline: false });
         });
+
+        await message.reply({ embeds: [embed] });
     },
 };
